f3f60b3e0620ff6d5bb0273031a385543891b490
Vbrav01 <victortosh001@gmail.com>
2023-05-23 16:51:40 +0300

    New change

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import "./App.css"
 import axios from 'axios';
 
 const App = () => {
@@ -51,41 +52,62 @@ const App = () => {
   }, [fromCurrency, toCurrency, amount]);
 
   return (
-    <form className="currency-form">
-      <h1>Currency Exchange App</h1>
-      <div className="form-group">
-        <label htmlFor="fromCurrency">From:</label>
-        <select id="fromCurrency" value={fromCurrency} onChange={handleFromCurrencyChange}>
-          <option value="">Select a currency</option>
-          {countries.map((country) => (
-            <option key={country} value={country}>
-              {country}
-            </option>
-          ))}
-        </select>
+    <>
+      <header className='header'>
+        <Header></Header>
+      </header>
+      <div className='main'>
+
+        <div className='left'>
+          <form className="currency-form">
+            <div className="form-group">
+              <label htmlFor="fromCurrency">From:</label>
+              <select id="fromCurrency" value={fromCurrency} onChange={handleFromCurrencyChange}>
+                <option value="">Select a currency</option>
+                {countries.map((country) => (
+                  <option key={country} value={country}>
+                    {country}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="form-group">
+              <label htmlFor="toCurrency">To:</label>
+              <select id="toCurrency" value={toCurrency} onChange={handleToCurrencyChange}>
+                <option value="">Select a currency</option>
+                {countries.map((country) => (
+                  <option key={country} value={country}>
+                    {country}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="form-group">
+              <label htmlFor="amount">Amount:</label>
+              <input type="number" id="amount" value={amount} onChange={handleAmountChange} />
+            </div>
+            {exchangeRate && (
+              <p>
+                {amount} {fromCurrency} = {exchangeRate * amount} {toCurrency}
+              </p>
+            )}
+          </form>
+        </div>
+        <div className='right'>
+
+        </div>
+
       </div>
-      <div className="form-group">
-        <label htmlFor="toCurrency">To:</label>
-        <select id="toCurrency" value={toCurrency} onChange={handleToCurrencyChange}>
-          <option value="">Select a currency</option>
-          {countries.map((country) => (
-            <option key={country} value={country}>
-              {country}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div className="form-group">
-        <label htmlFor="amount">Amount:</label>
-        <input type="number" id="amount" value={amount} onChange={handleAmountChange} />
-      </div>
-      {exchangeRate && (
-        <p>
-          {amount} {fromCurrency} = {exchangeRate * amount} {toCurrency}
-        </p>
-      )}
-    </form>
+    </>
+
   );
 };
+
+function Header() {
+  return (
+    <h1>Currency <span style={{ color: "#681396" }}>Exchange</span>  App</h1>
+
+  )
+}
 
 export default App;
